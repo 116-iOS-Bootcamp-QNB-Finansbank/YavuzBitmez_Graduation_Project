18cf371387660f84540655d2ba86e83ea37598bc
Yavuz Selim Bitmez <ybitmez@nettsi.com>
2021-10-01 18:47:10 +0300

    section struct created.

---
+++ Do me/Do me/ListScreen/Section.swift
@@ -0,0 +1,20 @@
+//
+//  Section.swift
+//  Do me
+//
+//  Created by Yavuz Selim Bitmez on 30.09.2021.
+//
+
+import Foundation
+
+struct Section {
+    let name: String
+//    let height: Int
+
+//    let rows: [Row]
+}
+
+struct Row {
+    let displayName: String
+    let downloads: Int
+}
